1d05fd6c7979b76251176e1a761761d7222a917f
Katharina Boser <boserkatharina@gmail.com>
2024-09-02 14:50:20 -0400

    Update Main.java

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -1,7 +1,16 @@
+/*
+   Programmer: Your Name
+   Date Created: Today’s Date
+   School: Your School
+*/
 class Main {
   public static void main(String[] args) {
     System.out.println((double)2/4+3);
     System.out.println((double)(2/4)+3);
     System.out.println((double)(2/4+3));
+    System.out.print(“Hello world! ”);
+		System.out.println(“Hooray! I did it.”);
   }
 }
+
+	
